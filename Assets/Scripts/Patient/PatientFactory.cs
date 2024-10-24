106a2ee576d47dc78c7d94331b0d80c39fb15a3b
Andrew Gabolinscy <oneyeway@gmail.com>
2024-10-24 14:59:18 +1300

    Added factory implementation

---
+++ Assets/Scripts/Patient/PatientFactory.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PatientName
+{
+    Freddy,
+    Foxy,
+    Chica,
+    Bonnie
+}
+
+public class PatientFactory : MonoBehaviour
+{
+    public static PatientFactory instance { get; private set; }
+    public GameObject patientPrefab;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public GameObject CreatePatient(PatientName _type, Vector3 _position)
+    {
+        GameObject patient = Instantiate(patientPrefab, _position, Quaternion.identity);
+        AttachPatientScript(_type, patient);
+        Debug.Log("A new Patient has entered the Hospital");
+        Debug.Log(patient.GetType());
+        return patient;
+    }
+
+    private void AttachPatientScript(PatientName _type, GameObject _patient)
+    {
+        switch (_type)
+        {
+            case PatientName.Freddy:
+                {
+                    _patient.AddComponent<Patient>();
+                    break;
+                }
+            case PatientName.Foxy:
+                {
+                    _patient.AddComponent<Patient>();
+                    break;
+                }
+            case PatientName.Chica:
+                {
+                    _patient.AddComponent<Patient>();
+                    break;
+                }
+            case PatientName.Bonnie:
+                {
+                    _patient.AddComponent<Patient>();
+                    break;
+                }
+        }
+    }
+}
